a6df9eb33a81a8daed9bd5a08ac7eddfbfded7a3
Martin Lundfall <martin.lundfall@protonmail.com>
2020-04-02 19:38:06 +0200

    Remaining opcodes adapted to symbolic stack, calldata and memory.
    A few tests displaying basic functionality in test/test.hs.
    Symbolic.startWithArgs can start a symbolic execution with pre and
    post conditions
    Storage locs and keys still forced to be concrete

--- src/hevm/src/EVM/ABI.hs
+++ src/hevm/src/EVM/ABI.hs
@@ -47,6 +47,7 @@ module EVM.ABI
   , encodeAbiValue
   , decodeAbiValue
   , parseTypeName
+  , sig
   ) where
 
 import EVM.Keccak (abiKeccak)
@@ -325,6 +326,9 @@ encodeAbiValue = BSLazy.toStrict . runPut . putAbi
 
 decodeAbiValue :: AbiType -> BSLazy.ByteString -> AbiValue
 decodeAbiValue = runGet . getAbi
+
+sig :: Text -> BS.ByteString
+sig s = BSLazy.toStrict . runPut $ putWord32be (abiKeccak (encodeUtf8 s))
 
 abiMethod :: Text -> AbiValue -> BS.ByteString
 abiMethod s args = BSLazy.toStrict . runPut $ do
